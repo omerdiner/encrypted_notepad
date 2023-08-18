38d65d5084466dc6748d7eae4fdcdc36dcd18dad
Omer Diner <omerdiner90@gmail.com>
2023-08-19 01:10:48 +0300

    decryption page functionality completed

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import messagebox, simpledialog
-
 import encryption
 import file_operations
 
-
+##TO-DO  MAKE ENCRIPTION TURKISH ALPHABET FRIENDLY
 
 def check_if_title_exists(title):
     titles = file_operations.get_titles()
@@ -62,13 +61,28 @@ def encrypt_note():
     file_operations.save_note_to_file(key_hint, encrypted_note, title)
     messagebox.showinfo(title="Note saved", message="Your note has been saved successfully!")
 ############################################################################
+def open_decrypted_note(title,key):
+
+    show_decrypted_note_window = tk.Toplevel(main_window)
+    show_decrypted_note_window.title(title)
+    show_decrypted_note_window.geometry("700x700")
+    show_decrypted_note_window.config(padx=20, pady=20)
+    show_decrypted_note_window.config(bg="dark blue")
+
+    note=file_operations.get_note_from_file(title)[1]
+    decrypted_note=encryption.decrypt(note,key)
+
+    decrypted_note_text = tk.Text(show_decrypted_note_window, height=20, width=50)
+    decrypted_note_text.config(font=("Arial", 24, "bold"))
+    decrypted_note_text.insert(tk.END, decrypted_note)
+    decrypted_note_text.pack()
+
 
 def open_decryption_window():
 
     if if_note_exists()==False:
        messagebox.showerror(title="Error", message="There are no notes to decrypt!")
        return
-
 
     decrypt_window = tk.Toplevel(main_window)
     decrypt_window.title("Decryption")
@@ -83,6 +97,7 @@ def open_decryption_window():
     titles.sort()
     listbox = tk.Listbox(decrypt_window, selectmode=tk.SINGLE)
     listbox.config(font=("Arial", 24, "bold"))
+    listbox.config(fg="dark gray")
     # change the color of the selected item
     listbox.config(selectbackground="red")
     listbox.pack(expand=True)
@@ -90,28 +105,53 @@ def open_decryption_window():
     for title in titles:
         listbox.insert(tk.END, title)
 
+    def callback(event):
+        selection = event.widget.curselection()
+        global clicked_title
+        if selection:
+            index = selection[0]
+            clicked_title = event.widget.get(index)
+
+        selected_hint=file_operations.get_note_from_file(clicked_title)[0]
+        hint_text="Hint : "+'"'+selected_hint+'"'
+        hint_label.config(text=hint_text)
+
+    listbox.bind("<<ListboxSelect>>", callback)
+
     hint_label = tk.Label(decrypt_window, text="Hint")
-    hint_label.config(font=("Arial", 24, "bold"))
+    hint_label.config(font=("Arial", 20, "bold"))
+    hint_label.config(padx=20, pady=20)
     hint_label.pack()
 
     key_label = tk.Label(decrypt_window, text="Key")
-    key_label.config(font=("Arial", 24, "bold"))
-    key_label.pack()
+    key_label.config(font=("Arial", 18, "bold"))
+    key_label.pack(side="left")
+    key_label.config(padx=20)
 
     key_entry = tk.Entry(decrypt_window)
     key_entry.config(font=("Arial", 24, "bold"))
-    key_entry.pack()
+    key_entry.pack(side="left")
+    key_entry.focus()
 
-    decrypt_button = tk.Button(decrypt_window, text="Decrypt")
+    decrypt_button = tk.Button(decrypt_window, text="Decrypt",command=lambda : open_decrypted_note(clicked_title,key_entry.get()))
     decrypt_button.config(font=("Arial", 24, "bold"))
     decrypt_button.pack()
 
-    ##TO-DO
-    # CLICKING ON THE TITLE FROM THE LIST WILL CHANGE THE HINT LABEL
-    # PRESSING DECRYPT WILL OPEN A NEW PAGE AND SHOW THE NOTE IN WHITE TEXT ON A DARK BACKGROUND
+    def delete_note(title):
+        answer = messagebox.askyesno(title="Delete",
+                                     message="Are you sure you want to delete the {}.txt ?".format(title))
+        if answer == False:
+            return
+        else:
+            file_operations.delete_file(title)
+            messagebox.showinfo(title="Note deleted", message="Your note has been deleted successfully!")
+            decrypt_window.destroy()
+
+    delete_button = tk.Button(decrypt_window, text="Delete",command=lambda : delete_note(clicked_title))
+    delete_button.config(font=("Arial", 24, "bold"))
+    delete_button.pack()
 
 
-#########################################################################
 
 main_window = tk.Tk()
 main_window.title("Top Secret")
